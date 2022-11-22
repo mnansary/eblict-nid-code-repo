cdc40638988814c8d992189a73be0b1e4648ef4f
mnansary <silentassasin0111@gmail.com>
2022-11-22 16:39:52 +0600

    gpu path

--- install_gpu.sh
+++ install_gpu.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 conda install cudatoolkit 
+conda install cudnn
 python -m pip install paddlepaddle-gpu==2.3.0.post111 -f https://www.paddlepaddle.org.cn/whl/linux/mkl/avx/stable.html
 pip uninstall protobuf
 pip install --no-binary protobuf protobuf==3.18.0
@@ -21,6 +22,8 @@ sudo chmod -R 777 weights/
 sudo chmod -R 777 tests/
 sudo touch logs.log
 sudo chmod 777 logs.log
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 python weights/download.py
 python setup_check.py 
 echo succeeded
